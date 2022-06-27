f1109fef8c22a530e7d379b5042878db46d305b1
bowler <1835597031@qq.com>
2022-06-27 23:13:48 +0800

    从src/main.js  打包到 lib/index.js

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    // new CleanWebpackPlugin(),
+  ],
+};
